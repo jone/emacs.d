40bb5e67ae28b3fda03b0eee791685c2a39798a8
Jonas Baumann <jone@jone.ch>
2018-11-03 15:19:29 +0630

    Always use spaces, no tabs.

--- modules/packages-cfg.el
+++ modules/packages-cfg.el
@@ -21,14 +21,14 @@
          ("M-y" . helm-show-kill-ring)
          ("C-x b" . helm-buffers-list))
   :bind (:map helm-map
-	      ("M-i" . helm-previous-line)
-	      ("M-k" . helm-next-line)
-	      ("M-I" . helm-previous-page)
-	      ("M-K" . helm-next-page)
-	      ("M-h" . helm-beginning-of-buffer)
-	      ("M-H" . helm-end-of-buffer))
+              ("M-i" . helm-previous-line)
+              ("M-k" . helm-next-line)
+              ("M-I" . helm-previous-page)
+              ("M-K" . helm-next-page)
+              ("M-h" . helm-beginning-of-buffer)
+              ("M-H" . helm-end-of-buffer))
   :config (progn
-	    (setq helm-buffers-fuzzy-matching t)
+            (setq helm-buffers-fuzzy-matching t)
             (helm-mode 1)))
 
 (use-package helm-descbinds
@@ -36,16 +36,16 @@
   :bind ("C-h b" . helm-descbinds))
 (use-package helm-files
   :bind (:map helm-find-files-map
-	      ("M-i" . nil)
-	      ("M-k" . nil)
-	      ("M-I" . nil)
-	      ("M-K" . nil)
-	      ("M-h" . nil)
-	      ("M-H" . nil)))
+              ("M-i" . nil)
+              ("M-k" . nil)
+              ("M-I" . nil)
+              ("M-K" . nil)
+              ("M-h" . nil)
+              ("M-H" . nil)))
 (use-package helm-swoop
   :ensure t
   :bind (("M-m" . helm-swoop)
-	 ("M-M" . helm-swoop-back-to-last-point))
+         ("M-M" . helm-swoop-back-to-last-point))
   :init
   (bind-key "M-m" 'helm-swoop-from-isearch isearch-mode-map))
 (use-package helm-ag
@@ -53,7 +53,7 @@
   :bind ("M-p" . helm-projectile-ag)
   :commands (helm-ag helm-projectile-ag)
   :init (setq helm-ag-insert-at-point 'symbol
-	      helm-ag-command-option "--path-to-agignore ~/.agignore"))
+              helm-ag-command-option "--path-to-agignore ~/.agignore"))
 
 (use-package projectile
   :ensure t
@@ -64,8 +64,8 @@
 (use-package helm-projectile
   :ensure t
   :bind (("C-x o" . helm-projectile-find-file)
-	 ("M-p" . helm-projectile-find-file)
-	 ("C-x r" . helm-projectile-grep))
+         ("M-p" . helm-projectile-find-file)
+         ("C-x r" . helm-projectile-grep))
   :config (helm-projectile-on))
 
 (use-package ruby-mode
@@ -73,7 +73,7 @@
   :defer t
   :mode (("\\.rb\\'"       . ruby-mode)
          ("\\.ru\\'"       . ruby-mode)
-	 ("\\.jbuilder\\'" . ruby-mode)
+         ("\\.jbuilder\\'" . ruby-mode)
          ("\\.gemspec\\'"  . ruby-mode)
          ("\\.rake\\'"     . ruby-mode)
          ("Rakefile\\'"    . ruby-mode)
@@ -82,10 +82,10 @@
          ("Capfile\\'"     . ruby-mode)
          ("Vagrantfile\\'" . ruby-mode))
   :config (progn
-	    (setq ruby-indent-level 2
-		  ruby-deep-indent-paren nil
-		  ruby-bounce-deep-indent t
-		  ruby-hanging-indent-level 2)))
+            (setq ruby-indent-level 2
+                  ruby-deep-indent-paren nil
+                  ruby-bounce-deep-indent t
+                  ruby-hanging-indent-level 2)))
 
 (use-package rubocop
   :ensure t
@@ -96,20 +96,20 @@
 (use-package minitest
   :bind ("M-e" . minitest-verify)
   :config (progn
-	    (defun minitest-ruby-mode-hook ()
-	      (tester-init-test-run #'minitest-run-file "_test.rb$")
-	      (tester-init-test-suite-run #'minitest-verify-all))
+            (defun minitest-ruby-mode-hook ()
+              (tester-init-test-run #'minitest-run-file "_test.rb$")
+              (tester-init-test-suite-run #'minitest-verify-all))
 
-	    (add-hook 'ruby-mode-hook 'minitest-mode)))
+            (add-hook 'ruby-mode-hook 'minitest-mode)))
 
 (use-package rspec-mode
   :ensure t
   :defer t
   :config (progn
-	    (defun rspec-ruby-mode-hook ()
-	      (tester-init-test-run #'rspec-run-single-file "_spec.rb$")
-	      (tester-init-test-suite-run #'rake-test))
-	    (add-hook 'ruby-mode-hook 'rspec-ruby-mode-hook)))
+            (defun rspec-ruby-mode-hook ()
+              (tester-init-test-run #'rspec-run-single-file "_spec.rb$")
+              (tester-init-test-suite-run #'rake-test))
+            (add-hook 'ruby-mode-hook 'rspec-ruby-mode-hook)))
 
 (use-package rbenv
   :ensure t
@@ -123,32 +123,32 @@
   :ensure t
   :defer 5
   :config (progn
-	    (global-flycheck-mode 1)
-	    ;; https://github.com/purcell/exec-path-from-shell
-	    ;; only need exec-path-from-shell on OSX
-	    ;; this hopefully sets up path and other vars better
-	    (exec-path-from-shell-initialize)
+            (global-flycheck-mode 1)
+            ;; https://github.com/purcell/exec-path-from-shell
+            ;; only need exec-path-from-shell on OSX
+            ;; this hopefully sets up path and other vars better
+            (exec-path-from-shell-initialize)
 
-	    ;; disable jshint since we prefer eslint checking
-	    (setq-default flycheck-disabled-checkers
-			  (append flycheck-disabled-checkers
-				  '(javascript-jshint)))
+            ;; disable jshint since we prefer eslint checking
+            (setq-default flycheck-disabled-checkers
+                          (append flycheck-disabled-checkers
+                                  '(javascript-jshint)))
 
-	    ;; use eslint with web-mode for jsx files
-	    (flycheck-add-mode 'javascript-eslint 'web-mode)
-	    (flycheck-add-mode 'javascript-eslint 'js2-mode)
+            ;; use eslint with web-mode for jsx files
+            (flycheck-add-mode 'javascript-eslint 'web-mode)
+            (flycheck-add-mode 'javascript-eslint 'js2-mode)
 
-	    ;; customize flycheck temp file prefix
-	    (setq-default flycheck-temp-prefix ".flycheck")
+            ;; customize flycheck temp file prefix
+            (setq-default flycheck-temp-prefix ".flycheck")
 
-	    ))
+            ))
 
 (use-package drag-stuff
   :ensure t
   :bind (("C-M-i" . drag-stuff-up)
-	 ("C-M-k" . drag-stuff-down)
-	 ("C-M-l" . python-indent-shift-right)
-	 ("C-M-j" . python-indent-shift-left)))
+         ("C-M-k" . drag-stuff-down)
+         ("C-M-l" . python-indent-shift-right)
+         ("C-M-j" . python-indent-shift-left)))
 
 (use-package magit
   :ensure t
@@ -172,19 +172,19 @@
 (use-package web-mode
   :ensure t
   :mode (("\\.erb\\'" . web-mode)
-	 ("\\.mustache\\'" . web-mode)
-	 ("\\.html?\\'" . web-mode)
+         ("\\.mustache\\'" . web-mode)
+         ("\\.html?\\'" . web-mode)
          ("\\.php\\'" . web-mode)
-	 ("\\.djhtml\\'" . web-mode)
-	 ("\\.djjson\\'" . web-mode)
-	 ("\\.scss\\'" . web-mode)
-	 ("\\.vue\\'" . web-mode))
+         ("\\.djhtml\\'" . web-mode)
+         ("\\.djjson\\'" . web-mode)
+         ("\\.scss\\'" . web-mode)
+         ("\\.vue\\'" . web-mode))
   :config (progn
             (setq web-mode-markup-indent-offset 2
-		  web-mode-css-indent-offset 2
-		  web-mode-code-indent-offset 2
-		  web-mode-script-padding 2
-		  web-mode-style-padding 2))
+                  web-mode-css-indent-offset 2
+                  web-mode-code-indent-offset 2
+                  web-mode-script-padding 2
+                  web-mode-style-padding 2))
   :init (bind-key "M-w d" 'django-web-mode))
 
 (use-package markdown-mode
@@ -195,28 +195,28 @@
   :ensure t
   :mode ("\\.coffee\\'" . coffee-mode)
   :config (progn
-	    (setq coffee-indent-tabs-mode nil
-		  coffee-tab-width 2)))
+            (setq coffee-indent-tabs-mode nil
+                  coffee-tab-width 2)))
 
 (use-package js2-mode
   :ensure t
   :mode (("\\.es6\\'" . js2-mode)
-	 ("\\.js\\'" . js2-mode))
+         ("\\.js\\'" . js2-mode))
   :config (progn
-	    (setq js2-basic-offset 2)))
+            (setq js2-basic-offset 2)))
 
 (use-package robe
   :ensure t
   :bind (("C-r C-j" . robe-jump)
-	 ("C-r C-r" . robe-rails-refresh)
-	 ("C-r C-s" . robe-start)))
+         ("C-r C-r" . robe-rails-refresh)
+         ("C-r C-s" . robe-start)))
 (add-hook 'ruby-mode-hook 'robe-mode)
 
 (use-package perspective
   :ensure t
   :bind (("C-p s" . persp-switch)
-	 ("C-p p" . persp-prev)
-	 ("C-p k" . persp-kill))
+         ("C-p p" . persp-prev)
+         ("C-p k" . persp-kill))
   :config (persp-mode))
 
 (use-package persp-projectile
